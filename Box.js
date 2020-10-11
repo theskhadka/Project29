91852d50b4d7cbb45bf0c73acf83082d654e5985
theskhadka <69537395+theskhadka@users.noreply.github.com>
2020-10-10 23:33:13 -0700

    Update Box.js

--- Box.js
+++ Box.js
@@ -4,6 +4,7 @@ class Box{
           restitution: 0.8
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
+      this.color = color(random(0, 255), random(0, 255), random(0, 255));
       this.width = width;
       this.height = height;
       World.add(world, this.body);
@@ -11,7 +12,7 @@ class Box{
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill(this.color);
       rect(pos.x, pos.y, this.width, this.height);
     }
 }
