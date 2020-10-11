f42c3441e9a3edafdc02511f2ee92dd5592d9877
theskhadka <69537395+theskhadka@users.noreply.github.com>
2020-10-10 23:44:00 -0700

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -10,9 +10,6 @@ class SlingShot{
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
-    attach(body){
-        this.sling.bodyA = body;
-    }
     
     fly(){
         this.sling.bodyA = null;
